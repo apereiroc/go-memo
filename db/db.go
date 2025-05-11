2c0fd475289e709807306c6f92b3b56e384770b0
Asier Pereiro <asier.pereiro.castro@cern.ch>
2025-05-11 23:32:29 +0200

    feat(db): correct handling of the database

--- db/db.go
+++ db/db.go
@@ -2,10 +2,13 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 
+	"github.com/apereiroc/go-memo/debug"
 	"github.com/apereiroc/go-memo/models"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -13,36 +16,93 @@ import (
 func InitDB() (*sql.DB, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		log.Fatal(err)
+		debug.Error(err)
+		log.Panic(err)
 	}
 
-	dbFile := path.Join(homeDir, ".config", "memo", "caca.db")
+	dbFile := path.Join(homeDir, ".config", "memo", "commands.db")
 
 	db, err := openDB(dbFile)
 	if err != nil {
 		return nil, err
 	}
-	return db, err
+	return db, nil
 }
 
 func openDB(path string) (*sql.DB, error) {
+	// check if path exists
+	fileInfo, pathErr := os.Stat(path)
+	if pathErr != nil && !os.IsNotExist(pathErr) {
+		return nil, pathErr
+	}
+
+	if pathErr == nil && fileInfo.IsDir() {
+		err := fmt.Errorf("%s is a directory", path)
+		debug.Error(err)
+		return nil, err
+	}
+
+	// open the DB
 	db, err := sql.Open("sqlite3", path)
 	if err != nil {
+		debug.Error(err)
 		return nil, err
 	}
 	db.SetMaxOpenConns(1)
 
+	// create the DB if it doesn't exist
+	if os.IsNotExist(pathErr) {
+		debug.Warnf("file %s did not exist; new DB file created", path)
+
+		// create directory
+		dir := filepath.Dir(path)
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			mkdirErr := fmt.Errorf("failed to create directories %s: %w", dir, err)
+			debug.Error(mkdirErr)
+			return nil, mkdirErr
+		}
+
+		// touch the DB to trigger file creation, if needed
+		if err := db.Ping(); err != nil {
+			debug.Error(err)
+			return nil, err
+		}
+	}
+
 	return db, nil
 }
 
+func isEmpty(db *sql.DB) (bool, error) {
+	// adapted from https://stackoverflow.com/questions/44098235/how-could-i-check-does-my-sqlite3-database-is-empty
+	sqlStatement := `
+	SELECT
+		COUNT(name)
+	FROM
+		sqlite_master;
+	`
+	row := db.QueryRow(sqlStatement)
+
+	var count int
+	err := row.Scan(&count)
+	if err != nil {
+		return false, err
+	}
+	return count == 0, nil
+}
+
 func LoadGroups(db *sql.DB) ([]models.Group, error) {
-	// no database, no groups
-	// valid scenario
-	if db == nil {
+	// check if the database is empty
+	empty, err := isEmpty(db)
+	if err != nil {
+		debug.Error(err)
+		return nil, err
+	}
+
+	if empty {
 		return []models.Group{}, nil
 	}
 
-	// i use left join to also get possible groups with no commands
+	// i use left join to get also possible groups with no commands
 	sqlStatement := `
 	SELECT
 		g.id, g.name,
@@ -58,6 +118,7 @@ func LoadGroups(db *sql.DB) ([]models.Group, error) {
 	`
 	rows, err := db.Query(sqlStatement)
 	if err != nil {
+		debug.Error(err)
 		return nil, err
 	}
 	defer rows.Close()
@@ -73,6 +134,7 @@ func LoadGroups(db *sql.DB) ([]models.Group, error) {
 
 		err = rows.Scan(&groupID, &groupName, &cmdText, &descText)
 		if err != nil {
+			debug.Error(err)
 			return nil, err
 		}
 
