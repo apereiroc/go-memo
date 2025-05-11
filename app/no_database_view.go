4b479ed1b1ef8e252841da70d4f185d100868b12
Asier Pereiro <asier.pereiro.castro@cern.ch>
2025-05-11 23:33:45 +0200

    feat: add empty-database view

---
+++ app/no_database_view.go
@@ -0,0 +1,22 @@
+package app
+
+import tea "github.com/charmbracelet/bubbletea"
+
+// Special screen to be displayed when the database is empty or not found
+type noDatabaseView struct{}
+
+func (v noDatabaseView) update(m model, msg tea.Msg) (viewStrategy, model, tea.Cmd) {
+	return v, m, nil
+}
+
+func (v noDatabaseView) view(m model) string {
+	return emptyDatabase
+}
+
+func (v noDatabaseView) next(m model) model {
+	return m
+}
+
+func (v noDatabaseView) prev(m model) model {
+	return m
+}
